9b9752474ae27cb6382416d116503054276d189d
Mikolaj Gierulski <mikolaj.gierulski@agora.pl>
2015-01-30 17:10:49 +0100

    defined default client request and connect timeout

--- articles-client/src/main/java/pl/com/agora/article/client/ArticleClientFactory.java
+++ articles-client/src/main/java/pl/com/agora/article/client/ArticleClientFactory.java
@@ -6,10 +6,10 @@ import pl.com.agora.api.client.rest.RestfulClientProxyFactory.ProxyConfiguration
 public class ArticleClientFactory {
 
     public ArticleClient createSimpleArticleClient(String host, int port) throws Exception {
-        return new RestfulClientProxyBuilder().withHost(host).withPort(port).withErrorClass(ArticleError.class).withDefaultExceptionClass(ArticleClientException.class).build(ArticleClient.class);        
+        return new RestfulClientProxyBuilder().withDefaultConnectTimeout(3000).withDefaultRequestTimeout(1000).withHost(host).withPort(port).withErrorClass(ArticleError.class).withDefaultExceptionClass(ArticleClientException.class).build(ArticleClient.class);        
     }
     
     public ArticleClient createCustomArticleClient(String host, int port, ProxyConfiguration config) throws Exception {
-        return new RestfulClientProxyBuilder().withHost(host).withPort(port).withConfig(config).withErrorClass(ArticleError.class).withDefaultExceptionClass(ArticleClientException.class).build(ArticleClient.class);
+        return new RestfulClientProxyBuilder().withDefaultConnectTimeout(3000).withDefaultRequestTimeout(1000).withHost(host).withPort(port).withConfig(config).withErrorClass(ArticleError.class).withDefaultExceptionClass(ArticleClientException.class).build(ArticleClient.class);
     }
 }
