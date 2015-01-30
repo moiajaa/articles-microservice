518fbd1305c1bfe94ee61bf118dc593295902165
Mikolaj Gierulski <mikolaj.gierulski@agora.pl>
2015-01-30 17:25:55 +0100

    fixed ArticleException class signature

--- articles-client/src/main/java/pl/com/agora/article/client/ArticleClientException.java
+++ articles-client/src/main/java/pl/com/agora/article/client/ArticleClientException.java
@@ -4,22 +4,18 @@ import pl.com.agora.api.client.rest.UriInvocationException;
 
 public class ArticleClientException extends UriInvocationException {
 
-    public ArticleClientException(String uri, String message, Object result, int responseStatus) {
+    public ArticleClientException(String uri, String message, ArticleError result, int responseStatus) {
         super(uri, message, result, responseStatus);
     }
 
-    public ArticleClientException(String uri, String message, Throwable cause, Object result, Integer responseStatus) {
+    public ArticleClientException(String uri, String message, Throwable cause, ArticleError result, Integer responseStatus) {
         super(uri, message, cause, result, responseStatus);
     }
 
     public ArticleClientException(String uri, String message) {
         super(uri, message);
     }
-
-    public ArticleClientException(String uri, Throwable cause) {
-        super(uri, cause);
-    }
-
+    
     @Override
     public ArticleError getResult() {
         return (ArticleError) super.getResult();
