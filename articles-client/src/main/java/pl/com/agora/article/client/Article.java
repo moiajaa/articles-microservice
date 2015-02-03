9751f2cf0d18d470fb8b487b27a4253ce3a0e047
Mikolaj Gierulski <mikolaj.gierulski@agora.pl>
2015-02-03 13:34:44 +0100

    dodanie hashcode, equals i tostring

--- articles-client/src/main/java/pl/com/agora/article/client/Article.java
+++ articles-client/src/main/java/pl/com/agora/article/client/Article.java
@@ -30,4 +30,47 @@ public class Article {
         this.content = content;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((content == null) ? 0 : content.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((title == null) ? 0 : title.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Article other = (Article) obj;
+        if (content == null) {
+            if (other.content != null)
+                return false;
+        } else if (!content.equals(other.content))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (title == null) {
+            if (other.title != null)
+                return false;
+        } else if (!title.equals(other.title))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Article [id=" + id + ", title=" + title + ", content=" + content + "]";
+    }
+
+    
 }
--- articles-client/src/main/java/pl/com/agora/article/client/ArticleError.java
+++ articles-client/src/main/java/pl/com/agora/article/client/ArticleError.java
@@ -30,4 +30,39 @@ public class ArticleError {
         this.message = message;
     }
 
+    @Override
+    public String toString() {
+        return "ArticleError [articleId=" + articleId + ", message=" + message + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((articleId == null) ? 0 : articleId.hashCode());
+        result = prime * result + ((message == null) ? 0 : message.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ArticleError other = (ArticleError) obj;
+        if (articleId == null) {
+            if (other.articleId != null)
+                return false;
+        } else if (!articleId.equals(other.articleId))
+            return false;
+        if (message == null) {
+            if (other.message != null)
+                return false;
+        } else if (!message.equals(other.message))
+            return false;
+        return true;
+    }
 }
